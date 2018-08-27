d5743ed756d864ca9eaa6af6256703e07f8ac670
pycc <dembelemassire34@gmail.com>
2018-08-27 08:49:19 +0000

    test commit

---
+++ test.rb
@@ -0,0 +1,32 @@
+class Person
+    @@teint = "noir"
+    def self.teint()
+     puts "a un teint #@@teint"
+    end
+        def initialize(nom,prenom)
+            @nom=nom
+            @prenom = prenom
+        end
+        def getNom()
+            @nom
+        end
+        def getPrenom()
+            @prenom
+        end
+        def setNom=(val)
+            @nom = val
+        end
+        def setPrenom=(val)
+            @prenom = val
+        end
+    end
+    moi = Person.new("mas","dem")
+    puts moi.getPrenom
+    puts moi.getNom
+    Person.teint()
+    
+    moi.setNom="massire"
+    moi.setPrenom="dembele"
+    puts moi.getPrenom
+    puts moi.getNom
+    Person.teint()
